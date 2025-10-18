944ae1d1ca91cf9ea209528416f25605096b1504
Zombie1995 <egorkerber1995@mail.ru>
2025-10-18 20:36:44 +0300

    formatting

--- src/main.py
+++ src/main.py
@@ -8,13 +8,16 @@ import difflib
 
 print(NOTES_DIR)
 
+
 def init_notes_folder(path):
     os.makedirs(path, exist_ok=True)
 
-init_notes_folder(NOTES_DIR)
+
+def init():
+    init_notes_folder(NOTES_DIR)
 
 
-# app = FastAPI(title="Versioned Notes API")
+app = FastAPI(title="Versioned Notes API")
 
 
 # @app.post("/notes/{note_id}")
@@ -38,6 +41,9 @@ init_notes_folder(NOTES_DIR)
 # @app.get("/notes/{note_id}/diff/{ver1}/{ver2}")
 # def get_diff(note_id: str, ver1: int, ver2: int):
 #     return {}
+
+init()
+
 
 def run():
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
